59f7fa58ee1dd677a7bfd785735dce8504ba3859
Maia <CIMA@gft.com>
2023-08-28 21:13:02 -0300

    feat(): setting the fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,88 @@
 import Home from "./src/pages/Home";
-import { Provider } from "react-redux";
+import { Provider as StoreProvider } from "react-redux";
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  configureFonts,
+} from "react-native-paper";
 import store from "./src/store";
+import themeConfig from "./src/config/theme.config";
 
-export default function App() {
+const fontConfig = {
+  web: {
+    regular: {
+      fontFamily: "sans-serif",
+      fontWeight: "normal",
+    },
+    medium: {
+      fontFamily: "sans-serif-medium",
+      fontWeight: "normal",
+    },
+    light: {
+      fontFamily: "sans-serif-light",
+      fontWeight: "normal",
+    },
+    thin: {
+      fontFamily: "sans-serif-thin",
+      fontWeight: "normal",
+    },
+  },
+  ios: {
+    regular: {
+      fontFamily: "sans-serif",
+      fontWeight: "normal",
+    },
+    medium: {
+      fontFamily: "sans-serif-medium",
+      fontWeight: "normal",
+    },
+    light: {
+      fontFamily: "sans-serif-light",
+      fontWeight: "normal",
+    },
+    thin: {
+      fontFamily: "sans-serif-thin",
+      fontWeight: "normal",
+    },
+  },
+  android: {
+    regular: {
+      fontFamily: "sans-serif",
+      fontWeight: "normal",
+    },
+    medium: {
+      fontFamily: "sans-serif-medium",
+      fontWeight: "normal",
+    },
+    light: {
+      fontFamily: "sans-serif-light",
+      fontWeight: "normal",
+    },
+    thin: {
+      fontFamily: "sans-serif-thin",
+      fontWeight: "normal",
+    },
+  },
+};
+
+const theme = {
+  ...DefaultTheme,
+  fonts: configureFonts({
+    config: {
+      fontFamily: "Ubuntu-Regular",
+      fontWeight: "normal",
+    },
+  }),
+};
+
+const App = (): JSX.Element => {
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <Home />
+      </PaperProvider>
+    </StoreProvider>
   );
-}
+};
+
+export default App;
